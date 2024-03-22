53d730b0868adcb3b0aca8fdbd03510d34e95a1b
Andriy Fedoryshyn <91829698+AndriyFedoryshyn@users.noreply.github.com>
2024-03-22 09:48:45 +0200

    lab 9

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
 
+//menu for user what task to run
+Console.WriteLine("What task do you want to run? (1, 2, 3)");
+int task = Convert.ToInt32(Console.ReadLine());
+
+switch (task)
+{
+    case 1:
+        Console.WriteLine("Task1");
+        Program1.Task1();
+        break;
+    case 2:
+        Console.WriteLine("Task2");
+        Program2.Task2();
+        break;
+    case 3:
+        Console.WriteLine("Task3");
+        Console.WriteLine("Enter 1 for Task1 or 2 for Task2");
+        int taskN = Convert.ToInt32(Console.ReadLine());
+        if (taskN == 1)
+            Program3.Task1();
+        else if (taskN == 2)
+            Program3.Task2();
+        else
+            Console.WriteLine("Invalid input");
+        break;
+    case 4:
+        Console.WriteLine("Task4");
+        Program4.Task4();
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
